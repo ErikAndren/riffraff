252d86fe4f97b857013d90920d3a542745780399
Erik Andrén Zachrisson <erik@zachrisson.info>
2021-01-24 16:10:01 +0100

    Use standard types

--- types.h
+++ types.h
@@ -11,15 +11,16 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
+#include <stdint.h>
 
 #include <unistd.h>
 //#	include <sys/mman.h>
 
-#include <dbglogger.h>
-#define LOG dbglogger_log
+//#include <dbglogger.h>
+#define LOG printf
 
 #include <sys/stat.h>
-#include <ppu-types.h>
+//#include <ppu-types.h>
 
 #if !defined(MAX_PATH)
 #	define MAX_PATH 260
@@ -34,24 +35,24 @@
 
 
 #define ES16(_val) \
-	((u16)(((((u16)_val) & 0xff00) >> 8) | \
-	       ((((u16)_val) & 0x00ff) << 8)))
+	((uint16_t)(((((uint16_t)_val) & 0xff00) >> 8) | \
+	       ((((uint16_t)_val) & 0x00ff) << 8)))
 
 #define ES32(_val) \
-	((u32)(((((u32)_val) & 0xff000000) >> 24) | \
-	       ((((u32)_val) & 0x00ff0000) >> 8 ) | \
-	       ((((u32)_val) & 0x0000ff00) << 8 ) | \
-	       ((((u32)_val) & 0x000000ff) << 24)))
+	((uint32_t)(((((uint32_t)_val) & 0xff000000) >> 24) | \
+	       ((((uint32_t)_val) & 0x00ff0000) >> 8 ) | \
+	       ((((uint32_t)_val) & 0x0000ff00) << 8 ) | \
+	       ((((uint32_t)_val) & 0x000000ff) << 24)))
 
 #define ES64(_val) \
-	((u64)(((((u64)_val) & 0xff00000000000000ull) >> 56) | \
-	       ((((u64)_val) & 0x00ff000000000000ull) >> 40) | \
-	       ((((u64)_val) & 0x0000ff0000000000ull) >> 24) | \
-	       ((((u64)_val) & 0x000000ff00000000ull) >> 8 ) | \
-	       ((((u64)_val) & 0x00000000ff000000ull) << 8 ) | \
-	       ((((u64)_val) & 0x0000000000ff0000ull) << 24) | \
-	       ((((u64)_val) & 0x000000000000ff00ull) << 40) | \
-	       ((((u64)_val) & 0x00000000000000ffull) << 56)))
+	((uint64_t)(((((uint64_t)_val) & 0xff00000000000000ull) >> 56) | \
+	       ((((uint64_t)_val) & 0x00ff000000000000ull) >> 40) | \
+	       ((((uint64_t)_val) & 0x0000ff0000000000ull) >> 24) | \
+	       ((((uint64_t)_val) & 0x000000ff00000000ull) >> 8 ) | \
+	       ((((uint64_t)_val) & 0x00000000ff000000ull) << 8 ) | \
+	       ((((uint64_t)_val) & 0x0000000000ff0000ull) << 24) | \
+	       ((((uint64_t)_val) & 0x000000000000ff00ull) << 40) | \
+	       ((((uint64_t)_val) & 0x00000000000000ffull) << 56)))
 
 /*
 #define LE16(_val) ((_val) & 0xffffu)
