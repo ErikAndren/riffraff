17165c485aa7bb6826590b426cea1e7b2e7c5a40
Erik Andrén Zachrisson <erik@zachrisson.info>
2021-01-26 15:51:37 +0100

    Allow for both rap2rif and rif2rap generation based on supplied file ending

--- main.c
+++ main.c
@@ -5,16 +5,17 @@ int main(int argc, char *argv[]) {
 	int status;
 	uint8_t idps[16];
 	const char *exdata_path = argv[2];
-	const char *rif_file = argv[3];
+	const char *rifrap_file = argv[3];
 	const char *out_path = argv[4];
+	char *rifrap_file_ext;
 
-	printf("Welcome to riffraff. Used to convert rif to rap files\n");
+	printf("Welcome to riffraff. Used to convert rif to rap files and vice versa\n");
 
 	if (argc != 5) {
 		printf("Invalid number of arguments\n");
 		printf("First argument is path to idps_key (16 bytes)\n");
 		printf("Second argument is path to where act.dat is located\n");
-		printf("Third argument is path to rif file\n");
+		printf("Third argument is path to rif/rap file\n");
 		printf("Fourth argument is the path where the generated rap is stored\n");
 
 		return -1;
@@ -32,14 +33,39 @@ int main(int argc, char *argv[]) {
 	printf("\n");
 
 	printf("Exdata path is: %s\n", exdata_path);
-	printf("Rif file is: %s\n", rif_file);
-	printf("Output directory is %s\n", out_path);
 
-	status = rif2rap(idps, exdata_path, rif_file, out_path);
-	if (status == 0) {
-		printf("Failed to create rap file\n");
+	if (strlen(rifrap_file) < 4) {
+		printf("rif/rap file is too short: %s\n", rifrap_file);
 		return -1;
 	}
-	printf("Rap file created in dir %s\n", out_path);
+
+	rifrap_file_ext = strrchr(rifrap_file, '.');
+	if (rifrap_file_ext == NULL) {
+		printf("Could not find file extension. Only .rif or .rap files are supported\n");
+		return -1;
+	}
+
+	if (strcmp(rifrap_file_ext, ".rif") == 0) {
+		printf("Rif file detected: %s. Will generate rap\n", rifrap_file);
+		status = rif2rap(idps, exdata_path, rifrap_file, out_path);
+		if (status == 0) {
+			printf("Failed to create rap file\n");
+			return -1;
+		}
+		printf("Rap file created in dir %s\n", out_path);
+	} else if (strcmp(rifrap_file_ext, ".rap") == 0) {
+		printf("Rap file detected: %s. Will generate rif\n", rifrap_file);
+		status = rap2rif(idps, exdata_path, rifrap_file, out_path);
+		if (status == 0) {
+			printf("Failed to create rif file\n");
+			return -1;
+		}
+		printf("Rif file created in dir %s\n", out_path);
+
+	} else {
+		printf("Failed to detect neither a .rif or .rap file: %s\n", rifrap_file);
+		return -1;
+	}
+
 	return 0;
 }
