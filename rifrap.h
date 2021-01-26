d048ea4ff0f87450f6fe9301a385e70fe71da21c
Erik Andrén Zachrisson <erik@zachrisson.info>
2021-01-26 15:51:03 +0100

    Add rap2rif function signature to header

--- rifrap.h
+++ rifrap.h
@@ -3,4 +3,6 @@
 
 int rif2rap(const uint8_t* idps_key, const char* exdata_path, const char* rif_file, const char* rap_path);
 
+int rap2rif(const uint8_t* idps_key, const char* exdata_path, const char* rap_file, const char *rif_path);
+
 #endif
